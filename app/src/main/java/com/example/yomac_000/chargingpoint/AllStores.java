f620086f890951914431ba7bfaeb5e6a2180e070
superzaky <superkytoz@gmail.com>
2015-12-20 01:14:18 +0100

    Busy with ArrayAdapter in AllStores.java

--- app/src/main/java/com/example/yomac_000/chargingpoint/AllStores.java
+++ app/src/main/java/com/example/yomac_000/chargingpoint/AllStores.java
@@ -1,22 +1,78 @@
 package com.example.yomac_000.chargingpoint;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import model.Store;
 import retrofit.Response;
 import services.StoreService;
 
 public class AllStores extends Activity {
+    private ListView lv;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Response<List<Store>> subprises = new StoreService().getSubprises();
-        System.out.println("AllStores (Activity) - subprise " + subprises);
         setContentView(R.layout.activity_all_stores);
+        lv = (ListView) findViewById(R.id.store_list);
+        Response<List<Store>> subprises;
+        try {
+            subprises = new StoreService().getSubprises();
+            Iterator it = subprises.body().iterator();
+            List<String> subprisesList = new ArrayList<String>();
+
+            int i = 0;
+            while(it.hasNext()) {
+                i++;
+                subprisesList.add((String) it.next());
+            }
+            StoreArrayAdapter stringArrayAdapter = new StoreArrayAdapter(
+                    getApplicationContext(),
+                    lv,
+                    subprisesList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
+    private class StoreArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StoreArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
 }
--- app/src/main/java/services/StoreService.java
+++ app/src/main/java/services/StoreService.java
@@ -1,10 +1,14 @@
 package services;
 
+import android.annotation.TargetApi;
 import android.os.AsyncTask;
+import android.os.Build;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import api.SubpriseAPI;
 import model.Store;
 import retrofit.Call;
@@ -24,25 +28,25 @@ public class StoreService {
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
+
+
     SubpriseAPI subpriseAPI = retrofit.create(SubpriseAPI.class);
 
-    public Response<List<Store>> getSubprises() {
-        new LongOperation().execute("");
-        System.out.println("getStores (StoreService.java) value "+ getStores());
-        return getStores();
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public Response<List<Store>> getSubprises() throws IOException, ExecutionException, InterruptedException {
+        LongOperation longOperation = new LongOperation();
+        longOperation.execute("");
+        stores = longOperation.get();
+        return stores;
     }
 
     private class LongOperation extends AsyncTask<String, Void, Response<List<Store>>> {
-
         @Override
         protected Response<List<Store>> doInBackground(String... params) {
-            System.out.println("doInBackground executed second");
+            //System.out.println("doInBackground executed second");
             try {
                 Call<List<Store>> call = subpriseAPI.listStores();
                 stores=call.execute();
-                Iterator it=stores.body().iterator();
-//                while(it.hasNext())
-//                    System.out.println(((Store)it.next()).getSTREET());
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -52,22 +56,12 @@ public class StoreService {
         @Override
         protected void onPreExecute() {
             //Can't put the call here because this is the main thread
-            System.out.println("onPreExecute first");
+            //System.out.println("onPreExecute first");
         }
 
         @Override
         protected void onPostExecute(Response<List<Store>> result) {
             //Can't put the call here because this is the main thread
-            setStores(stores);
         }
     }
-
-    public Response<List<Store>> getStores() {
-        return stores;
-    }
-
-    public void setStores(Response<List<Store>> stores) {
-        this.stores = stores;
-    }
-
 }
